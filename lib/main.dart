9d43e96f702925157a76f7b1a6988ac1263d9b1b
hoangchieu <chieuhv@scuti.asia>
2023-04-11 22:08:17 +0700

    install project

---
+++ lib/main.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+import 'package:project_home/main/presentation/project_home_app.dart';
+
+void main() {
+  runApp(const ProjectHomeApp());
+}
+
---
+++ lib/main/presentation/project_home_app.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+class ProjectHomeApp extends StatelessWidget {
+  const ProjectHomeApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const GetMaterialApp(
+    debugShowCheckedModeBanner: false,
+
+    );
+  }
+}
