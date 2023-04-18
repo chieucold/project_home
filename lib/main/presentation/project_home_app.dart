6a9c02315c8e47b4ed9efb8003939d8255c1dda6
chieucold <45590405+chieucold@users.noreply.github.com>
2023-04-18 23:36:03 +0700

    add branch develop (#1)
    Co-authored-by: hoangchieu <chieuhv@scuti.asia>

--- lib/main/presentation/project_home_app.dart
+++ lib/main/presentation/project_home_app.dart
@@ -1,14 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:project_home/feature/home/home_view.dart';
 
 class ProjectHomeApp extends StatelessWidget {
   const ProjectHomeApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return const GetMaterialApp(
-    debugShowCheckedModeBanner: false,
-
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: HomeView(),
     );
   }
 }
