754b8c1ecdc8d200d8e3cc80264ded0448f9fce4
chieucold <45590405+chieucold@users.noreply.github.com>
2023-05-24 18:02:37 +0700

    fix base (#10)
    Co-authored-by: ChieuHoang <chieuhv@scuti.asia>

--- lib/feature/dashboard/dash_board_view.dart
+++ lib/feature/dashboard/dash_board_view.dart
@@ -1,13 +1,40 @@
 import 'package:flutter/material.dart';
-import 'package:project_home/base/presentation/widget/screen_layout.dart';
+import 'package:get/get.dart';
+import 'package:project_home/base/presentation/resources/app_border_radius.dart';
+import 'package:project_home/base/presentation/resources/app_color.dart';
+import 'package:project_home/feature/dashboard/dash_board_controller.dart';
+import 'package:project_home/feature/dashboard/widget/button_navigation_bar.dart';
+import 'package:project_home/feature/home/home_view.dart';
 
-class DashBoardView extends StatelessWidget {
+class DashBoardView extends GetWidget<DashBoardController> {
   const DashBoardView({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return ScreenLayout(
-      body: Container(),
+    return Scaffold(
+      body: TabBarView(
+        physics: const NeverScrollableScrollPhysics(),
+        controller: controller.tabController,
+        children: const [
+          HomeView(),
+          Center(
+            child: Text('History'),
+          ),
+        ],
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+      floatingActionButton: FloatingActionButton(
+        shape: const RoundedRectangleBorder(
+          borderRadius: AppBorderRadius.circular10,
+        ),
+        onPressed: () {},
+        backgroundColor: Colors.black,
+        child: const Icon(
+          Icons.add_circle,
+          color: AppColor.color31C23F,
+        ),
+      ),
+      bottomNavigationBar: const ButtonsNavigationBar(),
     );
   }
 }
--- test/widget_test.dart
+++ test/widget_test.dart
@@ -5,10 +5,8 @@
 // gestures. You can also use WidgetTester to find child widgets in the widget
 // tree, read text, and verify that the values of widget properties are correct.
 
-
 import 'package:flutter_test/flutter_test.dart';
 import 'package:project_home/main/presentation/home_app.dart';
-
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
